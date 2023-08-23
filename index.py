50c877239228425d4cea23289a2b7d5d9ced4a0d
myusername <myemail>
2023-08-23 11:56:24 -0300

    Feature 0

---
+++ index.py
@@ -0,0 +1,55 @@
+import tweepy
+import discord
+import os
+
+# Configurações do Twitter
+TWITTER_API_KEY = "SUA_API_KEY_AQUI"
+TWITTER_API_SECRET = "SEU_API_SECRET_AQUI"
+TWITTER_ACCESS_TOKEN = "SEU_ACCESS_TOKEN_AQUI"
+TWITTER_ACCESS_SECRET = "SEU_ACCESS_SECRET_AQUI"
+TWITTER_ACCOUNT = "xetdaspromocoes"
+# TWITTER_ACCOUNT = "conta_do_twitter"
+
+# Configurações do Discord
+DISCORD_TOKEN = "LQ0PHJx5ptXV0ddpli3U03yZCHfpvPfr"
+DISCORD_CHANNEL_NAME = "geral"
+
+# Inicialização da API do Twitter
+auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET)
+auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_SECRET)
+twitter_api = tweepy.API(auth)
+
+# Inicialização do cliente do Discord
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Bot do Discord conectado como {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+# Função para buscar os tweets da conta do Twitter e postar no canal do Discord
+async def post_tweets():
+    channel = discord.utils.get(client.get_all_channels(), name=DISCORD_CHANNEL_NAME)
+    
+    if channel is not None:
+        tweets = twitter_api.user_timeline(screen_name=TWITTER_ACCOUNT, count=5, tweet_mode="extended")
+        for tweet in tweets:
+            tweet_text = tweet.full_text
+            tweet_url = f"https://twitter.com/{TWITTER_ACCOUNT}/status/1694347980457849342"
+            # tweet_url = f"https://twitter.com/{TWITTER_ACCOUNT}/status/{tweet.id}"
+            await channel.send(f"Novo tweet: {tweet_text}\nLink: {tweet_url}")
+    else:
+        print(f"Canal '{DISCORD_CHANNEL_NAME}' não encontrado.")
+
+# Executa o bot do Discord e posta os tweets no canal
+client.loop.create_task(post_tweets())
+client.run(DISCORD_TOKEN)
+
+
